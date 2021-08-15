afff9dc9236947e0a97b42a0739917cef353b4f0
Sam Powers <Sam.N.Powers@gmail.com>
2021-08-14 22:27:18 -0400

    No longer specify x

--- crl_alfred/alfred_demo_based_thor_env.py
+++ crl_alfred/alfred_demo_based_thor_env.py
@@ -52,7 +52,7 @@ class AlfredDemoBasedThorEnv(gym.Env):
 
         self.max_fails = max_fails  # see alfred.config.cfg_eval
 
-        self.env = ThorEnv(x_display=x_display)
+        self.env = ThorEnv()
 
         self.seed()
 
